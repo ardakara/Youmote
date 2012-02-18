2acbea37fe69761c8e597a85232d2001e0bf4141
Arda Kara <ardakara@gmail.com>
2012-02-17 18:51:11 -0800

    added skeleton code

---
+++ YoumoteController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using Microsoft.Research.Kinect.Nui;
+using Coding4Fun.Kinect.Wpf;
+
+namespace SkeletalTracking
+{
+    class YoumoteController : SkeletonController
+    {
+        public YoumoteController(MainWindow win) : base(win){}
+
+        public override void processSkeletonFrame(SkeletonData skeleton, Dictionary<int, Target> targets)
+        {
+
+            /* we'll call them here */
+
+        }
+
+        public override void controllerActivated(Dictionary<int, Target> targets)
+        {
+
+            /* YOUR CODE HERE */
+
+        }
+
+        // put your classifier code here as functions that return bool
+    }
+}
