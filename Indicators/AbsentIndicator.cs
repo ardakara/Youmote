d8eb48b910c8ab6129bee0e5e419312a6120aee5
Blake Carpenter <Blake@Blake-PC.(none)>
2012-02-21 23:14:24 -0800

    added absent indicator

---
+++ Indicators/AbsentIndicator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Research.Kinect.Nui;
+using Coding4Fun.Kinect.Wpf;
+
+namespace SkeletalTracking.Indicators
+{
+    public class AbsentIndicator
+    {
+
+        public Boolean isPositionDetected(SkeletonData skeleton)
+        {
+            if (skeleton == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
