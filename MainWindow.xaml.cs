bad9bcb297729172f78ba70dba47d965cefe74dc
Kenny Kao <kennyk616@gmail.com>
2012-03-03 15:41:42 -0800

    include speech stuff and debugger is working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,7 +28,8 @@ namespace YouMote
     using System.Threading;
     using Microsoft.Speech.AudioFormat;
     using Microsoft.Speech.Recognition;
-    using System.IO; 
+    using System.IO;
+    using YouMote.Speech;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -72,143 +73,13 @@ namespace YouMote
         public float k_yMaxJointScale = 3.0f;
         private static readonly int Bgr32BytesPerPixel = (PixelFormats.Bgr32.BitsPerPixel + 7) / 8;
 
-        //audio stuff
-        KinectAudioSource audioSource;
-        SpeechRecognitionEngine sre;
-        Stream stream;
+        private SpeechRecognizer mySpeechRecognizer;
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             SetupKinect();
             youmoteController = new YoumoteController(this);
             currentController = youmoteController;
-
-            
-            audioSource = nui.AudioSource;
-            audioSource.EchoCancellationMode = EchoCancellationMode.None; // No AEC for this sample
-            audioSource.AutomaticGainControlEnabled = false; // Important to turn this off for speech recognition
-            this.sre = this.CreateSpeechRecognizer();
-        }
-
-        private SpeechRecognitionEngine CreateSpeechRecognizer()
-        {
-            RecognizerInfo ri = GetKinectRecognizer();
-            if (ri == null)
-            {
-                MessageBox.Show(
-                    @"There was a problem initializing Speech Recognition.
-Ensure you have the Microsoft Speech SDK installed.",
-                    "Failed to load Speech SDK",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error);
-                this.Close();
-                return null;
-            }
-
-            SpeechRecognitionEngine sre;
-            try
-            {
-                sre = new SpeechRecognitionEngine(ri.Id);
-            }
-            catch
-            {
-                MessageBox.Show(
-                    @"There was a problem initializing Speech Recognition.
-Ensure you have the Microsoft Speech SDK installed and configured.",
-                    "Failed to load Speech SDK",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error);
-                this.Close();
-                return null;
-            }
-
-            var grammar = new Choices();
-            grammar.Add("hi");
-            grammar.Add("hello");
-            grammar.Add("What's up");
-            grammar.Add("Sup");
-            grammar.Add("Hey");
-
-            var gb = new GrammarBuilder { Culture = ri.Culture };
-            gb.Append(grammar);
-
-            // Create the actual Grammar instance, and then load it into the speech recognizer.
-            var g = new Grammar(gb);
-
-            sre.LoadGrammar(g);
-            sre.SpeechRecognized += this.SreSpeechRecognized;
-            sre.SpeechHypothesized += this.SreSpeechHypothesized;
-            sre.SpeechRecognitionRejected += this.SreSpeechRecognitionRejected;
-
-            return sre;
-        }
-
-
-        private void RejectSpeech(RecognitionResult result)
-        {
-            string status = "Rejected: " + (result == null ? string.Empty : result.Text + " " + result.Confidence);
-            targets[0].setTargetText(status);
-        }
-
-        private void SreSpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
-        {
-            this.RejectSpeech(e.Result);
-        }
-
-        private void SreSpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
-        {
-            targets[0].setTargetText("hypothesize as: " + e.Result.Text);
-            //this.ReportSpeechStatus("Hypothesized: " + e.Result.Text + " " + e.Result.Confidence);
-        }
-
-        private void SreSpeechRecognized(object sender, SpeechRecognizedEventArgs e)
-        {
-            SolidColorBrush brush;
-
-            if (e.Result.Confidence < 0.5)
-            {
-                this.RejectSpeech(e.Result);
-                return;
-            }
-
-            targets[0].setTargetText("recognize as: " + e.Result.Text);
-
-/*            switch (e.Result.Text.ToUpperInvariant())
-            {
-                case "RED":
-                    brush = this.redBrush;
-                    break;
-                case "GREEN":
-                    brush = this.greenBrush;
-                    break;
-                case "BLUE":
-                    brush = this.blueBrush;
-                    break;
-                case "CAMERA ON":
-                    System.Diagnostics.Process.Start("notepad.exe");
-                    this.kinectColorViewer1.Visibility = System.Windows.Visibility.Visible;
-                    brush = this.blackBrush;
-                    break;
-                case "CAMERA OFF":
-                    this.kinectColorViewer1.Visibility = System.Windows.Visibility.Hidden;
-                    brush = this.blackBrush;
-                    break;
-                default:
-                    brush = this.blackBrush;
-                    break;
-            }
- */
-        }
-
-        private static RecognizerInfo GetKinectRecognizer()
-        {
-            Func<RecognizerInfo, bool> matchingFunc = r =>
-            {
-                string value;
-                r.AdditionalInfo.TryGetValue("Kinect", out value);
-                return "True".Equals(value, StringComparison.InvariantCultureIgnoreCase) && "en-US".Equals(r.Culture.Name, StringComparison.InvariantCultureIgnoreCase);
-            };
-            return SpeechRecognitionEngine.InstalledRecognizers().Where(matchingFunc).FirstOrDefault();
         }
 
         private void SetupKinect()
@@ -236,18 +107,20 @@ Ensure you have the Microsoft Speech SDK installed and configured.",
                 nui.Start();
 
                 //need 4 seconds for kinect speech to be ready
-                
+               
                 int wait = 4;
                 while (wait > 0)
                 {
-                    //targets[0].setTargetText("Device will be ready for speech recognition in" + wait +  "second(s).\r");
+ //                   this.DebugSpeechTextBox.Text = "Device will be ready for speech recognition in" + wait +  "second(s).";
                     wait--;
                     Thread.Sleep(1000);
                 }
 
                 //add event to receive skeleton data
                 nui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
-
+                this.mySpeechRecognizer = new SpeechRecognizer(this);
+//                this.mySpeechRecognizer.SaidSomething += this.RecognizerSaidSomething;
+                this.mySpeechRecognizer.Start(nui.AudioSource);
 
             }
         }
