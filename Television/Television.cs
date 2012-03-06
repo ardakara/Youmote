412cdce6b57c5a574976fc4176822cc8bbda44f2
Blake Carpenter <Blake@Blake-PC.(none)>
2012-03-05 21:03:40 -0800

    fixed channel change bug

--- Television/Television.cs
+++ Television/Television.cs
@@ -13,7 +13,7 @@ namespace YouMote.Television
         // Blake's Path For Quick Copying:  "C:\\Users\\Blake\\Documents\\CS247\\P4\\Youmote\\Video\\";
         // Charlton's path for quick copying: "C:\\Users\\Charlton Soesanto\\Desktop\\Youmote\\Video\\"
         // Kenny's path for quick copying: "C:\\Users\\Kenny\\CS247\\Youmote\\Video\\"
-        private static String VIDEO_PATH = "C:\\Users\\Kenny\\CS247\\Youmote\\Video\\";
+        private static String VIDEO_PATH = "C:\\Users\\Blake\\Documents\\CS247\\P4\\Youmote\\Video\\";
         private double _volume = 1.0;
         public double Volume
         {
@@ -116,20 +116,31 @@ namespace YouMote.Television
             this.updateChannelListings();
             if (this.CurrentChannelIndex >= 0)
             {
-                if (!this._cachedMedia.Equals(Media.NULL_MEDIA) && this.CurrentChannelIndex == CACHE_CHANNEL_ID)
+
+                if (!this._cachedMedia.Equals(Media.NULL_MEDIA) && this.CurrentChannelIndex == 0)
                 {
-                    this.CurrentChannelIndex = this.CurrentChannelIndex - 1;
+                    // if current channel is 0, and the cache media isnt null
+                    // show the cached media
+                    this.CurrentChannelIndex = Television.CACHE_CHANNEL_ID;
+
                     Media nextMedia = this._cachedMedia;
                     this._screenController.moveMediaToRight(nextMedia);
+                    return true;
                 }
-                else
+                else if (this._currentChannelIndex - 1 >= 0 && this._currentChannelIndex - 1 < this._channels.Count)
                 {
+                    // otherwise show the next media if it is still in range
                     this.CurrentChannelIndex = this.CurrentChannelIndex - 1;
                     Channel nextChannel = this._channels[this.CurrentChannelIndex];
                     Media nextMedia = nextChannel.Media;
                     this._screenController.moveMediaToRight(nextMedia);
+                    return true;
                 }
-                return true;
+                else
+                {
+                    return false;
+                }
+
             }
             else
             {
